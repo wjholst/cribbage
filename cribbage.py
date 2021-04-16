f19531c1557b1f8e0ba833be52b82983c182f135
wjholst <wjholst@gmail.com>
2021-04-16 06:39:47 -0600

    Update as of 4/16

--- cribbage.py
+++ cribbage.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask import render_template, session,request, flash, make_response
 from datetime import datetime
 import json
+#import cribbage_utilities
 from cribbage_utilities import *
-from cribbage_utilities import init_score_lookup
+#from cribbage_utilities import init_score_lookup
 import re
 
 app = Flask(__name__, static_folder="static")
@@ -36,7 +37,8 @@ def startGame():
     if player_is_dealer:
         # who = 'Player'
         
-        who = 'You deal' + session['id']
+        # who = 'You deal' + session['id']
+        who = 'Player deals'    
     else:
         who = 'Computer deals'
 
@@ -46,22 +48,25 @@ def startGame():
         computer_cardFileName="static/CardDeck/" + computer_cardname,
         who_deals=who))
     resp.set_cookie('dealer_is', who)
-    
+    resp.set_cookie('computer_score', '0')
+    resp.set_cookie('player_score', '0')
+    resp.set_cookie('narration_1', 'Play 3, pair for 2 - count is 28')
+    resp.set_cookie('narration_2', 'Play 2 - go for 1')
     
     return resp
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
+# @app.route("/hello/")
+# @app.route("/hello/<name>")
+# def hello_there(name = None):
+#     return render_template(
+#         "hello_there.html",
+#         name=name,
+#         date=datetime.now()
+#     )
 
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
+# @app.route("/api/data")
+# def get_data():
+#     return app.send_static_file("data.json")
 
 @app.route("/about/")
 def about():
@@ -84,6 +89,8 @@ def select_cards():
     computer_hand = h.get_computer_hand()
     print (computer_hand)
     starter = h.get_starter_card()
+    starter = str(starter[0])
+    print ('starter is ' + str(starter))
     print ('request method is ' +str(request.method))
 
     if request.method == 'POST':
@@ -97,15 +104,13 @@ def select_cards():
             flash ('Select 2 cards for crib..')
 
         print (cardlist)
-
-
-
-        
+        #chand,comp_crib = pick_best_hand(computer_hand,True)
+        #print (chand)
     resp = make_response (render_template(
         "select_cards.html",
         #player_cardFileName="static/CardDeck/" + player_cardname,
-        blank = "static/" + "card_back.svg",
-        transparent = "static/" + "transparent.svg",
+        blank = "static/CardDeck/" + "Card-Back.svg",
+        transparent = "static/CardDeck/" + "transparent.svg",
         pcn0 = "static/CardDeck/" + player_cardnames[0],
         pcn1 = "static/CardDeck/" + player_cardnames[1],
         pcn2 = "static/CardDeck/" + player_cardnames[2],
@@ -123,19 +128,90 @@ def select_cards():
     ph = json.dumps(player_hand.tolist())
     ch = json.dumps(computer_hand.tolist())
     resp.set_cookie('playerhand',ph)
-    resp.set_cookie('computerhand',ch)      
+    resp.set_cookie('computerhand',ch)
+    resp.set_cookie('selecting_crib','y') 
+    resp.set_cookie('starter',starter)       
     return resp
 
-@app.route("/crib", methods=['GET', 'POST'])
-def crib():
-    print ("in crib")
+@app.route("/play_cards", methods=['GET', 'POST'])
+def play_cards():
+    print ("in play_cards")
+    crib = []
+    ch = []
+    selecting_crib = request.cookies.get('selecting_crib')
+    ph = request.cookies.get('playerhand')
+    starter = int(request.cookies.get('starter'))
+    who_deals = request.cookies.get('dealer_is')
+    player_deals = who_deals == "Player deals"
+    
+    playerhand = json.loads(ph)
+    playerhand = list(playerhand)
+    player_hand = []
+    print (playerhand)
+    
+    print ('selecting crib value is ' + selecting_crib)
     if request.method == 'POST':
-        print ('in post')
-        
-        cardlist = request.form.getlist("cardNumber")
-        print (cardlist)
+      print ('in post')
+      print ('selecting crib value is ' + selecting_crib)
+      if selecting_crib=='y':  
+        criblist = request.form.getlist("cardNumber")
+        print (criblist)
+        player_crib = [int(i) for i in criblist]
 
-    return render_template("crib.html")
+        c_hand,c_crib = pick_best_hand(computer_hand,player_deals)
+        print (c_hand)
+        print (c_crib)
+        playerlist = list(set(playerhand).difference(set(player_crib)))
+        player_hand = [int(i) for i in playerlist]
+        crib_list = player_crib + c_crib
+        #res = make_response()
+
+        ch = json.dumps(c_hand)
+        crib = json.dumps(crib_list)
+      else:
+        print ('not slecting crib')
+    #print ('player hand ' + (player_hand))
+    print(type(player_hand)) 
+    print (type(player_hand[0]))
+    player_cardnames = [card_lookup[cn] for cn in player_hand]
+    starter_cardname = "static/CardDeck/" + card_lookup[starter] 
+    cm_crib = "static/CardDeck/" + "Card-Back.svg"
+    pl_crib = "static/CardDeck/" + "Card-Back.svg"
+
+    if player_deals:
+        starterc = "static/CardDeck/transparent.svg"
+        starterp = starter_cardname
+        cm_crib = "static/CardDeck/transparent.svg"
+        start_msg = "Click to start play"
+    else:
+        starterp = "static/CardDeck/transparent.svg"
+        starterc = starter_cardname
+        pl_crib = "static/CardDeck/transparent.svg"
+        start_msg = "Select and play"
+    resp = make_response (render_template(
+        "play_cards.html",
+        blank = "static/CardDeck/" + "Card-Back.svg",
+        pc0 = player_hand[0],
+        pc1 = player_hand[1],
+        pc2 = player_hand[2],
+        pc3 = player_hand[3],
+        pcn0 = "static/CardDeck/" + player_cardnames[0],
+        pcn1 = "static/CardDeck/" + player_cardnames[1],
+        pcn2 = "static/CardDeck/" + player_cardnames[2],
+        pcn3 = "static/CardDeck/" + player_cardnames[3],
+        ccrib = cm_crib, 
+        pcrib = pl_crib,
+        computer_starter = starterc,
+        player_starter = starterp,
+        start_msg = start_msg ))
+    if selecting_crib=='y':  
+        resp.set_cookie("computer_hand",value=ch)
+        resp.set_cookie("crib_hand",value=crib)
+        ph = json.dumps(player_hand)
+        resp.set_cookie("player_hand",value=ph)
+
+    resp.set_cookie("selecting_crib",value='n')
+    return resp
 
 @app.route("/test", methods=['GET','POST']) 
 def test(): 
--- cribbage_utilities.py
+++ cribbage_utilities.py
@@ -10,6 +10,8 @@ import random
 import pickle
 import numpy as np
 import os
+from itertools import combinations 
+import pandas as pd
 
 cards = []
 card_filenames = []
@@ -90,8 +92,12 @@ class Hands:
 def init_score_lookup ():
 
     if os.path.isfile('OnlyOnVisualStudio.txt'):
-        score_lookup_4 = pickle.load( open( "C:\\Users\\wj\\Documents\\cribbage\\static\\score_lookup_4.p", "rb" ))
-        score_lookup_5 = pickle.load( open( "C:\\Users\\wj\\Documents\\cribbage\\static\\score_lookup_5.p", "rb" ))
+        if os.path.isfile('wjh_on_visual_studio.txt'):
+            score_lookup_4 = pickle.load( open( "C:\\Users\\wj\\Documents\\cribbage\\static\\score_lookup_4.p", "rb" ))
+            score_lookup_5 = pickle.load( open( "C:\\Users\\wj\\Documents\\cribbage\\static\\score_lookup_5.p", "rb" ))
+        else:
+            score_lookup_4 = pickle.load( open( "C:\\Users\\Walt\\OneDrive\\Documents\\Programming\\Python\\Cribbage\\static\\score_lookup_4.p", "rb" ))
+            score_lookup_5 = pickle.load( open( "C:\\Users\\Walt\\OneDrive\\Documents\\Programming\\Python\\Cribbage\\static\\score_lookup_5.p", "rb" ))    
     else:
         score_lookup_4 = pickle.load( open( "/home/cribbage152154/mysite/static/score_lookup_4.p", "rb" ))
         score_lookup_5 = pickle.load( open( "/home/cribbage152154/mysite/static/score_lookup_5.p", "rb" ))
@@ -113,5 +119,302 @@ def test_deal():
     print (computer_hand)
     print (starter)
 
+def sum_combinations (comb):
+    lsum = 0
+    for c in comb:
+        if np.sum(c) == 15:
+            lsum += 2 
+    return lsum
+
+def score_15 (hand):
+    hand = np.array(hand)
+    #print ('in score 15, hand is ' + str(hand))
+    #new_hand = np.array(hand)
+    # discard the suit
+    new_hand = hand % 100
+    # convert JQK to 10
+    new_hand[new_hand > 10] = 10
+    sum_15 = 0
+    for i in range(2,6):
+        comb = combinations(new_hand,i)
+        sum_15 += sum_combinations (comb)
+    return sum_15                    
+
+def is_run (arr):
+    is_run = True
+    prior = arr[0]
+    for a in arr[1:]:
+        consecutive = (prior + 1) == a
+        is_run = is_run and consecutive
+        #print (is_run)
+        prior = a.copy()
+    return is_run
+#a = np.array ([4,5,6,7])
+#print (is_run(a))
+
+def score_run (hand):
+    new_hand = hand % 100
+    sortedn = np.sort(new_hand)
+    sum_comb = 0
+    if (len(sortedn) == 5) and is_run(sortedn):
+        return 5
+    for c in combinations(sortedn,4):
+        if is_run(c):
+            sum_comb += 4
+    if sum_comb == 0: # there are no 4 way combinations
+        for c in combinations(sortedn,3):
+            if is_run(c):
+                sum_comb += 3
+    return sum_comb  
+
+def score_pairs (hand):
+    new_hand = hand % 100
+    sum_pairs = 0
+    for c in combinations(new_hand,2):
+        if c[0] == c[1]:        
+            sum_pairs += 2
+    return sum_pairs    
+
+def score_flush (hand):
+    suits = hand // 100
+    #print (suits)
+    if len(hand) == 5:
+        result = len(set(suits)) == 1  
+        if result:
+            return 5
+    result = len(set(suits[:4])) == 1  
+    if result:
+        return 4
+    else:
+        return 0
+
+def score_nobs (hand):
+    if len(hand) < 5:
+        return 0
+    starter = hand[4]
+    if starter % 100 == 11:
+        return 0 #heels    
+    # starter card is last in list
+    starter_suit = hand[4] // 100
+    #print (starter_suit)
+
+    jacks = []
+    for c in hand:
+        if c % 100 == 11:
+            jacks.append(c)
+    #print (jacks)
+    if len(jacks) == 0:
+        return 0
+    else:    
+        suits = np.array(jacks) // 100
+        is_nobs = False
+        for s in suits:
+            is_nobs = is_nobs or (s == starter_suit)
+        return int(is_nobs)
+#hnd = [103,204,311,411,410]        
+#print (score_nobs (hnd))       
+
+def score_all (hand):
+    total = 0
+    s15 = score_15(hand)
+    runs = score_run(hand)
+    pairs = score_pairs(hand)
+    flush = score_flush(hand)
+    nobs = score_nobs(hand)
+    total = s15 + runs + pairs + flush + nobs
+    return s15, runs, pairs, flush, nobs, total
+
+def score_all_numeric (hand):
+    total = 0
+    s15 = score_15(hand)
+    runs = score_run(hand)
+    pairs = score_pairs(hand)
+    total = s15 + runs + pairs
+    return s15, runs, pairs, total
+
+def create_deck ():
+    deck = []
+    for s in range (1,5):
+        for n in range (1,14):
+            deck.append (s*100 + n)
+    deck_set = set(deck)
+    return deck, deck_set
+deck, deck_set = create_deck()
+
+def score_mean (tmp_hand, full_hand):
+    all_scores = []
+    #print ('for tmp hand ' + str(tmp_hand))
+    remaining = deck_set.difference(set(full_hand))     
+    for c in remaining:
+        #tmp = tmp_hand.copy()
+        tmp = np.append(tmp_hand, c)
+        (s15, runs, pairs, flush, nobs, total) = score_all (tmp)
+        #print (total)
+        all_scores.append (total)
+
+#    print ('mean score is ' + str(np.mean(all_scores)))
+    return np.mean(all_scores) 
+        
+
+def score_crib (tmp_hand,full_hand,use_full_hand):
+    all_scores = []
+
+    if use_full_hand:
+        remaining = deck_set.difference(set(full_hand))
+    else:
+        remaining = deck_set.difference(set(tmp_hand))    
+    for c in combinations(remaining,2):
+
+        tmp = np.append(tmp_hand,c)
+        (s15, runs, pairs, flush, nobs, total) = score_all (tmp)       
+        all_scores.append (total)
+        #print (total)    
+    return np.mean(all_scores), all_scores
+
+#make a list from key
+
+def key_to_list (key):
+    length = round((len(str(key)) + 0.001)/2)
+    #print (length)
+    lst = []
+    rem = key
+    for i in reversed (range(length)):
+        digit = rem // 100**i
+        rem = key % (100**i)
+        lst.append (digit)
+    return np.array(lst)
+
+# make a key from a list
+
+def list_to_key (lst):
+    k = 0
+    for i, l in enumerate(reversed(lst)):
+
+        k += l * (100**i)
+    return k    
+# call like this: k = list_to_key (sorted([13,12,2,3,4]))       
+
 card_lookup, card_lookup_reverse = init_cards()
 score_lookup_4, score_lookup_5 = init_score_lookup()
+
+'''
+There are 2 cards passed to the crib, plus the potential starter, plus the remaining cards in the deck after the 6 card hand.
+
+'''
+def evaluate_crib_w_starter (cards, starter, remaining):
+    i = 0
+    l_remaining = remaining.difference ([starter])
+    #    print (l_remaining)
+    score_list = []
+    for c in combinations(l_remaining,2):
+        hand = []
+        #print (c)
+        hand = list(cards) + [starter] + list(c)
+        #print (hand)
+        hand = np.array(hand) % 100
+        #print (hand)
+        
+        score = score_lookup_5[list_to_key(sorted(hand))] + score_flush(hand) + score_nobs(hand)
+
+        score_list.append(score)
+        i += 1
+    
+    return (np.mean(score_list))   
+"""
+This function differs in that the starter is not passed. Instead 3 random cards
+added to the crib from the remaining cards. This is not quite as accurate, but
+should suffice, given that 46-C-3 hands are evaluated.
+"""
+def evaluate_crib_wo_starter (cards, remaining):
+
+    #    print (l_remaining)
+    #print ('crib cards are ' + str(cards))
+    score_list = []
+    for c in combinations(remaining,3):
+        hand = []
+        #print (c)
+        hand = list(cards) + list(c)
+        #print (hand)
+        hand = (np.array(hand) % 100)
+        #print (hand)
+        
+        score = score_lookup_5[list_to_key(sorted(hand))] + score_flush(hand) + score_nobs(hand)
+
+        score_list.append(score)
+    #print (len(score_list))
+    
+    return (np.mean(score_list))   
+
+if False: # this is test code only
+    hnd = [101,203,204,205,307,308]
+    hd = [101,203,204,205]
+    remaining = deck_set.difference(hnd)
+    crib = set(hnd).difference(set(hd))  
+    for i in range(15):
+        for s in remaining:
+    
+            evaluate_crib_w_starter (crib,s,remaining)
+
+    print (evaluate_crib_wo_starter(crib,remaining))
+
+def evaluate_hand (hand, ):
+    remaining = deck_set.difference (set(hand))
+    results = []
+    for c in combinations(hand,4):
+        crib = set(hand).difference (c)
+        score_list = []
+        for r in remaining:
+            hnd = []
+            hnd = list(c) + [r]
+            hnd = np.array(hnd) % 100
+            sorted_hnd = sorted(hnd)
+            #print (sorted_hnd)
+            score = score_lookup_5[list_to_key(sorted_hnd)] + score_flush(hnd) + score_nobs(hnd)
+            score_list.append (score)
+        #print ('hand:' + str(hnd))        
+        hand_score = np.mean (score_list)
+
+        #print ('crib:'+str(crib))
+        crib_score = evaluate_crib_wo_starter(crib,remaining) 
+        lcrib = list(crib)
+
+        #print (crib_score)
+        results.append ([list(c), list(crib), hand_score, crib_score, hand_score+crib_score, hand_score-crib_score])
+    results = pd.DataFrame(results)
+    results.columns = ['Hand','Crib','Hand Score','Crib Score','Dealer','Opponent']
+    return results
+
+
+computer_hand = [401,201,305,208,313,213]
+
+
+def pick_best_hand (hand, player_crib):
+    computer_crib = not player_crib
+    evaluation = evaluate_hand(hand)
+    if computer_crib:
+        print ('computer crib')
+        crib_owner = 'C'
+        #print ('computer hand/crib cards are ')
+        best = evaluation.sort_values('Dealer',ascending=False).head(5)
+        #print (best)
+        hnd = best['Hand']
+
+        crb = best['Crib']
+        cr = [c for c in crb]
+        
+    else:
+        crib_owner = 'P'
+        #print ('player hand/crib cards are ')
+        best = evaluation.sort_values('Opponent',ascending=False).head(5)
+        #print (best)
+    hnd = best['Hand']
+    crb = best['Crib']
+    cr = [c for c in crb]
+    #print (hnd)
+    hd = [h for h in hnd]
+    #print (hd[0])
+    cr[0]
+    return hd[0], cr[0]
+computer_hand = ([104,105,206,307,205,308])
+computer_hand = [401,201,305,208,313,213]
+#print (pick_best_hand(computer_hand,player_crib=True))
